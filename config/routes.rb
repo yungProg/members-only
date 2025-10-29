d25a2548fd9f3a6a103da7b58c11f8fef9078e71
yungProg <gboakye007@outlook.com>
2025-10-29 16:51:13 +0000

    member can sign out

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "posts#index"
 
-  resources :post, only: [ :index, :new, :create ]
+  resources :posts, only: [ :index, :new, :create ]
 end
