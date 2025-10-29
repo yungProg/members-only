743b9eecaec7c01626171f6128bf26286f5bf13e
yungProg <gboakye007@outlook.com>
2025-10-29 15:16:01 +0000

    build posts controller logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,28 @@
 class PostsController < ApplicationController
+  before_action :authenticate_member, only: [ :new, :create ]
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if current_member.posts.create(post_params)
+      flash[:notice] = "Post sent!"
+      redirect_to root_url
+    else
+      flash.now[:alert] = "Error while sending post"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
