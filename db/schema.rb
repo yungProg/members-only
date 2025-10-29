d001340dde79b7d19c1ebb27932592172483b106
yungProg <gboakye007@outlook.com>
2025-10-29 15:05:31 +0000

    generate Post(s) model and controller and routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_29_144858) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_29_150317) do
   create_table "members", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -22,4 +22,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_29_144858) do
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.integer "member_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["member_id"], name: "index_posts_on_member_id"
+  end
+
+  add_foreign_key "posts", "members"
 end
